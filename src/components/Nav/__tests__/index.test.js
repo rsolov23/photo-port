056d3b221d054c668e5d103e11fd55ef3fe80de2
Rachel Solov <rsolov23@yahoo.com>
2021-07-30 16:46:19 -0500

    preparing to deploy

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,21 +5,23 @@ import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
 const categories = [
-  { name: 'portraits', description: 'Portraits of people in my life' }
-]
+  { name: "portraits", description: "Portraits of people in my life" },
+];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
 const mockContactSelected = jest.fn();
 const mockSetContactSelected = jest.fn();
-it('renders', () => {
-  render(<Nav
-    categories={categories}
-    setCurrentCategory={mockSetCurrentCategory}
-    currentCategory={mockCurrentCategory}
-    contactSelected={mockContactSelected}
-    setContactSelected={mockSetContactSelected}
-  />);
-})
+it("renders", () => {
+  render(
+    <Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />
+  );
+});
 afterEach(cleanup);
 
 describe("Nav component", () => {
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
@@ -14,24 +14,34 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+            <a
+              data-testid="about"
+              href="#about"
+              onClick={() => setContactSelected(false)}
+            >
               About me
             </a>
           </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <li className={`mx-2 ${contactSelected && "navActive"}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
+                currentCategory.name === category.name &&
+                !contactSelected &&
+                "navActive"
+              }`}
               key={category.name}
             >
               <span
